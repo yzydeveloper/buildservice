7b2179ee29ae35b30fae9aafa7ceafe00c057cde
yangzhaoyu <wardw@qq.com>
2021-03-06 17:55:01 +0800

    Modified   app.js Modified   index.html

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-const { download } = require("./common/build");
-const gitPath =
-  "http://admin:yxiBPPswzD3YpJS_aABJ@49.234.49.103:9090/root/test.git"; //远端git地址
-//   "http://admin:yxiBPPswzD3YpJS_aABJ@49.234.49.103:9090/root/test.git";
-const temp = gitPath.split("/");
-const ext = temp[temp.length - 1]; //提取目标文件夹的名称
-global.gitPath = gitPath;
-global.ext = ext;
-async function run() {
-  await download();
-}
-run();
+// const { download } = require("./common/build");
+// const gitPath =
+//   "http://admin:yxiBPPswzD3YpJS_aABJ@49.234.49.103:9090/root/test.git"; //远端git地址
+// //   "http://admin:yxiBPPswzD3YpJS_aABJ@49.234.49.103:9090/root/test.git";
+// const temp = gitPath.split("/");
+// const ext = temp[temp.length - 1]; //提取目标文件夹的名称
+// global.gitPath = gitPath;
+// global.ext = ext;
+// async function run() {
+//   await download();
+// }
+// run();
