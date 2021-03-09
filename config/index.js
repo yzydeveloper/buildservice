7dd06e149b0bce1b374a46009ed6de59deab85a4
yzydeveloper <wqrdw@qq.com>
2021-03-10 00:00:19 +0800

    Modified   config/index.js

--- config/index.js
+++ config/index.js
@@ -1,7 +1,7 @@
 module.exports = Object.freeze({
-  SERVER_PATH: "49.234.49.103", // ssh地址 服务器地址
-  SSH_USER: "root", // ssh 用户名
-  PASSWORD: "YZYdxf20000722", // 用密码连接服务器
+  SERVER_PATH: "", // ssh地址 服务器地址
+  SSH_USER: "", // ssh 用户名
+  PASSWORD: "", // 用密码连接服务器
   PATH: "/data/www", // 需要上传的服务器目录地址 如 /data/www/
   APPLICATION_PORTAL: "/buildservice/gitroot",
   SITE_DATA: [
